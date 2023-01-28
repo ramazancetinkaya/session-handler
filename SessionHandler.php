8b214e3373877a2f9d3a40ab1ef9cbf1fa89d0c0
Ramazan Çetinkaya <75214035+ramazancetinkaya@users.noreply.github.com>
2023-01-28 18:43:50 +0300

    Update SessionHandler.php

--- SessionHandler.php
+++ SessionHandler.php
@@ -2,21 +2,55 @@
 /**
  * SessionHandler
  *
- * Copyright (c) [2023] [Ramazan Çetinkaya]
- * 
  * @author [ramazancetinkaya] (https://github.com/ramazancetinkaya)
  * @date [28.01.2023]
  *
+ * Copyright (c) [2023] [Ramazan Çetinkaya]
+ * 
+ * Software License Agreement
+ * 
+ *  1. Ownership and Intellectual Property Rights
+ * 
+ *      The Author retains all rights, title, and interest in and to the Software, including all intellectual property rights. The Software is protected by copyright laws and international copyright treaties, as well as other intellectual property laws and treaties. The Author grants you a non-transferable, non-exclusive, limited license to use the Software in accordance with the terms and conditions of this Agreement.
+ *
+ *  2. Permitted Use
+ *
+ *      The Software is licensed for use in open source and private projects. 
+ *
+ *      - For open source projects, the Software may be used under the terms and conditions of a compatible open source license.
+ *      - For private projects, the Software may be used with the prior written permission of the Author. To request permission, please contact the Author at [github.com/ramazancetinkaya].
+ *
+ *  3. Restrictions
+ *
+ *      Avoid doing these.
+ *       
+ *      - Modify, translate, reverse engineer, decompile, or disassemble the Software, except to the extent that such activity is expressly permitted by applicable law;
+ *      - Rent, lease, lend, sell, redistribute, or sublicense the Software;
+ *      - Remove any proprietary notices or labels on the Software;
+ *      - Use the Software in any manner that infringes the intellectual property rights of the Author or any third party.
+ *
+ *  4. Disclaimer of Warranties
+ *
+ *      The Software is provided "AS IS," without warranty of any kind, express or implied, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose. The Author does not warrant that the Software will meet your requirements or that the operation of the Software will be uninterrupted or error-free.
+ *
+ *  5. Limitation of Liability
+ *
+ *      To the maximum extent permitted by applicable law, in no event shall the Author be liable for any direct, indirect, incidental, special, or consequential damages arising out of or in connection with the use of the Software, even if the Author has been advised of the possibility of such damages.
+ *
+ *
  * Software Ownership and Attribution
  *
- * As the author of this software, I retain ownership of the code and any associated intellectual property. However, I am happy to make the software available for use in open source projects, under the following conditions:
+ *  As the author of this software, I retain ownership of the code and any associated intellectual property. However, I am happy to make the software available for use in open source and private projects, under the following conditions:
  *
- * 1. Attribution: Any use of the software in a public or open source project must clearly indicate my name as the original author of the code. This includes, but is not limited to, including my name in the software's documentation, comments, and any accompanying materials.
- * 2. Open Source: The software may be used in open source projects, as long as the project is also open source and uses a compatible open source license.
- * 3. No Warranty: The software is provided "as is," without warranty of any kind, express or implied. I will not be liable for any damages arising from the use of the software.
- * 4. No Liability: I will not be liable for any damages resulting from the use of the software, including but not limited to any direct, indirect, special, incidental, or consequential damages.
+ *      1. Attribution: Any use of the software in a public or open source project must clearly indicate my name and my Github profile link (https://github.com/ramazancetinkaya) as the original author of the code. This includes, but is not limited to, including my name in the software's documentation, comments, and any accompanying materials.
+ *      2. Open Source: The software may be used in open source projects, as long as the project is also open source and uses a compatible open source license.
+ *      3. Private Use: The software may also be used in private projects, with my express permission. If you would like to use the software in a private project, please contact me to request permission.
+ *      4. No Warranty: The software is provided "as is," without warranty of any kind, express or implied. I will not be liable for any damages arising from the use of the software.
+ *      5. No Liability: I will not be liable for any damages resulting from the use of the software, including but not limited to any direct, indirect, special, incidental, or consequential damages.
  *
- * By using this software, you agree to these terms and conditions. If you have any questions or concerns, please contact me.
+ *
+ *  By installing, copying, or otherwise using the Software, you agree to be bound by the terms of this Agreement. If you do not agree to the terms of this Agreement, do not install or use the Software. 
+ *  If you have any questions or concerns, please contact me.
  */
 
 class SessionHandler implements \SessionHandlerInterface
